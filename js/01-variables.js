b1c8c56b477a5da9dadc8cac44843e893023a849
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-08-28 08:58:22 -0300

    Agregamos un ejemplo de comentarios y como dejar mensajes

---
+++ js/01-variables.js
@@ -0,0 +1,21 @@
+// Esto es un comentario
+
+/*
+Esto es un comentario
+de 
+multiples lineas
+*/
+
+// Como mandar mensajes
+
+console.log("hola mundo");
+console.info("hola mundo");
+console.warn("hola mundo");
+console.error("hola mundo");
+
+document.writeln("hola mundo");
+document.writeln("<h1 class='tex-blue'>hola mundo</h1>");
+
+// ventanas emergentes
+
+// alert("practica de alert");
