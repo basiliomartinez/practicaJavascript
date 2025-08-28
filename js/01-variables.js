57271f7fa5ee28caa941441bbcb44294b48027c0
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-08-28 16:11:58 -0300

    Agregamos ejemplo  de variable const

--- js/01-variables.js
+++ js/01-variables.js
@@ -22,3 +22,9 @@ document.writeln("<h1 class='tex-blue'>hola mundo</h1>");
 
 // variables var-let-const
 
+const taiga ='https://tree.taiga.io/project/earias08-comision-web16/us/74?kanban-include_attachments=1&kanban-include_tasks=1&kanban-status=10453682'
+
+console.log(taiga)
+document.writeln('<p>'+taiga+'</p>')
+document.writeln(taiga)
+document.writeln('un nuevo texto')
