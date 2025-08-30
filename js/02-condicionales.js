f0c280da2674205d416b626a63e41e35aeed8531
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-08-30 09:00:48 -0300

    Agregamos la logica del ejercicio para saber si somos o no mayores

---
+++ js/02-condicionales.js
@@ -0,0 +1,26 @@
+// Condicionales simples
+/*
+If (condicion) {
+Codigo a ejecutar si se cumple la condicion
+}
+*/
+
+// Condicionales dobles
+/*
+If (condicion) {
+Codigo a ejecutar si se cumple la condicion
+} else {
+ Codigo a ejecutar si no cumple la condicion}
+*/
+
+// Pedir al usuario una edad y decir si es mayor de edad o no
+// 18 años es mayor de edad
+
+let edad= parseInt(prompt ('ingresa tu edad'))
+console.log(edad)
+
+if (edad>=18){
+    document.writeln('Sos mayor de edad 😎')
+} else{
+    document.writeln('Sos menor de edad 👑')
+}
