974be821fdf3298d2e0a80803c4ef29c0aeed53f
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-09-02 20:21:34 -0300

    Ultimos cambios al ejercio de descuento

--- js/02-PracticaLetConst.js
+++ js/02-PracticaLetConst.js
@@ -5,6 +5,7 @@ let precioFinal = precioProducto;
 if (precioProducto > 2000) {
   const descuento = precioProducto * 0.25;
   precioFinal = precioProducto - descuento;
+  document.writeln(`El descuento del producto es $ ${descuento}`);
 }
 
-document.writeln(`El precio del producto es $ ${precioFinal}`);
+document.writeln(`<br>El precio del producto es $ ${precioFinal}`);
